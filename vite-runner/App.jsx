e6d53fb9458bf6568090c6291a8d625d6e8ff6b3
longdahai <longdahai@bytedance.com>
2024-03-24 17:27:57 +0800

    feat: fiber

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -1,11 +1,35 @@
 
-/** @jsx CReact.createElement */
 import React from './core/React.js';
 
 const App = React.createElement(
   'div',
   { id: 'app', class: 'app' },
-  '144'
+  '22222222222222222244'
 );
+
+
+// function app() {
+//   return /*#__PURE__*/ React.createElement(
+//     "div",
+//     {
+//       id: "3242",
+//       class: "3"
+//     },
+//     /*#__PURE__*/ React.createElement(
+//       "div",
+//       {
+//         id: "3242"
+//       },
+//       /*#__PURE__*/ React.createElement(
+//         "div",
+//         null,
+//         "            ",
+//         /*#__PURE__*/ React.createElement("div", null, "3333")
+//       )
+//     )
+//   );
+// }
+
+
 
 export default App;
--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -49,35 +49,146 @@ const createElement = (type, props, ...children) => {
 
 // 1. 根据app 这个dom 对象，转化为dom 节点，需要实现一个render 函数
 
+// const render = (el, container) => {
+//   const dom =
+//     el.type === 'TEXT_ELEMENT'
+//       ? document.createTextNode('')
+//       : document.createElement(el.type);
+
+//   // 给dom 设置属性
+
+//   const _props = el.props;
+
+//   Object.keys(_props).forEach(key => {
+//     if (key !== 'children') {
+//       dom[key] = _props[key];
+//     }
+//   });
+
+//   // 处理children
+//   const _children = el.props.children;
+
+//   _children.forEach(child => {
+//     render(child, dom);
+//   });
+
+//   container.append(dom);
+// };
+let nextFiberOfUnit = null;
+
 const render = (el, container) => {
-  const dom =
-    el.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(el.type);
+  nextFiberOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+  performFiberOfUnit(nextFiberOfUnit);
+};
 
-  // 给dom 设置属性
+const createDom = type => {
+  return type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(type);
+};
 
-  const _props = el.props;
-
-  Object.keys(_props).forEach(key => {
+const updateDomProps = (dom, props) => {
+  Object.keys(props).forEach(key => {
     if (key !== 'children') {
-      dom[key] = _props[key];
+      dom[key] = props[key];
     }
   });
-
-  // 处理children
-  const _children = el.props.children;
-
-  _children.forEach(child => {
-    render(child, dom);
-  });
-
-  container.append(dom);
 };
+
+const initChild = Fiber => {
+  const children = Fiber.props.children;
+  let preChild = null;
+
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: Fiber,
+      sibling: null,
+      dom: null,
+    };
+
+    if (index === 0) {
+      Fiber.child = newFiber;
+    } else {
+      preChild.sibling = newFiber;
+    }
+
+    preChild = newFiber;
+  });
+};
+
+const performFiberOfUnit = fiber => {
+  // 0. 如果没有dom，再创建
+  if (!fiber.dom) {
+    //1. 创建dom
+    const dom = createDom(fiber.type);
+    fiber.dom = dom;
+    fiber.parent.dom.append(dom);
+    //2. 给 dom 设置属性props
+    updateDomProps(dom, fiber.props);
+  }
+
+  //3. 找一下child 节点
+
+  initChild(fiber);
+
+  //4. 子节点找完，找兄弟节点
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  // 5，返回兄弟节点
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+  //6 兄弟节点没有了，返回父节点
+
+  return fiber.parent.sibling;
+};
+
+const fiberLoop = deadline => {
+  //1. 创建dom
+  let shouldYield = false;
+
+  while (!shouldYield && nextFiberOfUnit) {
+    nextFiberOfUnit = performFiberOfUnit(nextFiberOfUnit);
+    console.log('111', deadline.timeRemaining());
+    if (deadline.timeRemaining() < 1) {
+      shouldYield = true;
+    }
+    requestIdleCallback(fiberLoop);
+  }
+};
+
+requestIdleCallback(fiberLoop);
 
 const React = {
   render,
   createElement,
 };
+
+// function hello() {
+//   const a = 1;
+//   return /*#__PURE__*/ React.createElement(
+//     'div',
+//     null,
+//     /*#__PURE__*/ React.createElement('div', null, {
+//       a,
+//     }),
+//     /*#__PURE__*/ React.createElement('div', null, {
+//       a,
+//     }),
+//     /*#__PURE__*/ React.createElement('div', null, {
+//       a,
+//     })
+//   );
+// }
 
 export default React;
